545248feaef0637f13c31798c86cce67553e5529
adnansehanovic <sehanovic.adnan@gmail.com>
2026-05-11 18:47:36 +0200

    skripta.js
    u ovom komitu sam dodao skriptu sa index.html. Dodana je skripta za odbrojavanje vremena do utakmice koja funkcionise da principu da primi od korisnika/mene informaciju kada je utakmice te pamti uzima trenutno vrijeme, oduzima vrijeme utakmice od trenutno vremena, odvaja dane sate minute sekunde u posebne kategorije te ispisuje vrijeme do utakmice. Takodjer je dodana skripta za dark mode dugme

--- js/skripta.js
+++ js/skripta.js
@@ -2,6 +2,86 @@
 
 
 
+/*                      Index html                        */
+
+
+
+/* -------------------------------------------------------- */
+    //Ovdje upiati datum i vrijeme utakmice (mjesec dan godina sati:ninute:sekunde)
+    const datumUtakmice = new Date("May 24, 2026 17:30:00").getTime(); // tajmer odbrojava do tog vremena
+
+    //funkcija koja se pokrece svake sekunde
+    const tajmer = setInterval(function() {
+
+        //uzima trenutno vrijeme
+        const sada = new Date().getTime();
+
+        //racuna razliku od trenutnog vremena do vremena utakmice
+        const udaljenost = datumUtakmice - sada;
+
+        //matematicke kalkulacije za dan sat minuta sekunda
+        const dani = Math.floor(udaljenost / (1000 * 60 * 60 * 24));
+        const sati = Math.floor((udaljenost % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minute = Math.floor((udaljenost % (1000 * 60 * 60)) / (1000 * 60));
+        const sekunde = Math.floor((udaljenost % (1000 * 60)) / 1000);
+
+        //ispisivanje rezultata
+        //dodajemo 0 ako je npr 9 dana pisati je 09
+        document.getElementById("dani").innerHTML = dani < 10 ? "0" + dani : dani;
+        document.getElementById("sati").innerHTML = sati < 10 ? "0" + sati : sati;
+        document.getElementById("minute").innerHTML = minute < 10 ? "0" + minute : minute;
+        document.getElementById("sekunde").innerHTML = sekunde < 10 ? "0" + sekunde : sekunde;
+
+        //zaustavljanje brojaca nakon sto istekne vrijeme
+        if (udaljenost < 0) {
+            clearInterval(tajmer); // Zaustavlja brojač
+            document.getElementById("dani").innerHTML = "00";
+            document.getElementById("sati").innerHTML = "00";
+            document.getElementById("minute").innerHTML = "00";
+            document.getElementById("sekunde").innerHTML = "00";
+        }
+    }, 1000); // 1000 milisekundi = 1 sekunda
+
+
+// DARK MODE DUGME 
+
+const modDugme = document.getElementById('mod-dugme');
+const body = document.body;
+
+//da li je korisnik prsli put ostavio tamni mod upaljen
+if (localStorage.getItem('tema') === 'tamna') {
+    body.classList.add('tamni-mod');
+    modDugme.textContent = '☀️'; // Postavljamo sunce jer je tamni mod aktivan
+}
+
+//dodavanje ili skidanje klsae tamni mod
+modDugme.addEventListener('click', () => {
+    body.classList.toggle('tamni-mod');
+    
+    //provjeravanje da li je tamni mod upaljne
+    if (body.classList.contains('tamni-mod')) {
+        localStorage.setItem('tema', 'tamna');
+        modDugme.textContent = '☀️'; //prebacit na sunce
+    } else {
+        localStorage.setItem('tema', 'svijetla');
+        modDugme.textContent = '🌙'; //ako nije vrati na mjesec
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------------------------------------------- */
+
+
+
 /*                      Kontakt html                        */
 
 
